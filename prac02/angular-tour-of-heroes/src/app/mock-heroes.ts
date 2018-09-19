4ae569bfabee53d543d29a0b49bb3d49fef5f4be
dalbamm <oscar.yoon13@gmail.com>
2018-09-19 20:03:43 +0900

    practice02 complete

---
+++ prac02/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+	{id: 11, name: 'Mr.Nice', initial: false},
+	{id: 12, name: 'Narco', initial: false},
+	{id: 13, name: 'Bombasto', initial: false},
+	{id: 14, name: 'Celeritas', initial: false},
+	{id: 15, name: 'Magneta', initial: false},
+	{id: 16, name: 'RubberMan', initial: false},
+	{id: 17, name: 'Dynama', initial: false},
+	{id: 18, name: 'Dr IQ', initial: false},
+	{id: 19, name: 'Magma', initial: false},
+	{id: 20, name: 'Tornado', initial: false}
+	
+];
