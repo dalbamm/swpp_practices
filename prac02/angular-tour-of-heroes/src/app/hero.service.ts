1cece7e3af821420d0006da8b6b301a6d5941128
dalbamm <oscar.yoon13@gmail.com>
2018-09-27 00:57:35 +0900

    slide 29 done

--- prac02/angular-tour-of-heroes/src/app/hero.service.ts
+++ prac02/angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,17 +3,33 @@ import { MessageService } from './message.service';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs' ;
+import { delay, map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
   constructor(private messageService: MessageService ) { }
-	
+
   getHeroes(): Observable<Hero[]> { this.messageService.add('HeroService: fetched heroes');
-  	return of(HEROES);
+  	return of(HEROES).pipe(delay(1000), 
+			map((heroes: Hero[]) =>
+				heroes.map((hero:Hero)=>({...hero, initial: false}) ) ),
+			);
   }
-
+/*
+	getHeroes(): Promise<Hero[]> {
+		return new Promise((resolve, reject) => {
+				if(condition) {
+					resolve(this.getHeroes())
+				} else{
+					reject('ERROR MSG')
+				}
+				});
+	}
+	getHeroes().then(heroes => {
+				This.heroes = heroes;
+			}).catch(err => console.log(err));
+*/
 }
-
-
