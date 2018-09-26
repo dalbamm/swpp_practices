6b88ea6a8af9bf5f8f417259d2afd5b197ade2c4
dalbamm <oscar.yoon13@gmail.com>
2018-09-26 19:29:17 +0900

    Session3 25th slide done

--- prac02/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ prac02/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HEROES} from '../mock-heroes';
 import {Hero} from '../hero';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,12 +14,16 @@ export class HeroesComponent implements OnInit {
 		name: 'Windstrom'
 		};*/
 heroes = HEROES;
-  constructor() { }
+  constructor(private heroService: HeroService) { }
   ngOnInit() {
+  	this.getHeroes();
   }
   	selectedHero: Hero;
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;	
 	}
-	
+	getHeroes(): void {
+		this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+//<!--		this.heroes = this.heroService.getHeroes();-->
+	}
 }
