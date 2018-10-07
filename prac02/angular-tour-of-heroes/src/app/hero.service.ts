6bcd21808fc519ea28de9431414c3ef638655737
dalbamm <oscar.yoon13@gmail.com>
2018-10-07 19:58:39 +0900

    slide 40 done..

--- prac02/angular-tour-of-heroes/src/app/hero.service.ts
+++ prac02/angular-tour-of-heroes/src/app/hero.service.ts
@@ -12,12 +12,17 @@ export class HeroService {
 
   constructor(private messageService: MessageService ) { }
 
-  getHeroes(): Observable<Hero[]> { this.messageService.add('HeroService: fetched heroes');
+  /*getHeroes(): Observable<Hero[]> { this.messageService.add('HeroService: fetched heroes');
   	return of(HEROES).pipe(delay(1000), 
 			map((heroes: Hero[]) =>
 				heroes.map((hero:Hero)=>({...hero, initial: false}) ) ),
 			);
+  }*/
+ getHeroes(): Observable<Hero> { 
+	 this.messageService.add('HeroService: fetched hero id=${id}');
+	 return of(HEROES.find(hero => hero.id === id));
   }
+
 /*
 	getHeroes(): Promise<Hero[]> {
 		return new Promise((resolve, reject) => {
@@ -26,10 +31,10 @@ export class HeroService {
 				} else{
 					reject('ERROR MSG')
 				}
-				});
-	}
+			});
+	
 	getHeroes().then(heroes => {
 				This.heroes = heroes;
 			}).catch(err => console.log(err));
-*/
+	}*/
 }
--- prac02/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ prac02/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,21 +9,12 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-/*	hero: Hero = {
-		id: 1,
-		name: 'Windstrom'
-		};*/
 heroes = HEROES;
   constructor(private heroService: HeroService) { }
   ngOnInit() {
   	this.getHeroes();
   }
-  	selectedHero: Hero;
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;	
-	}
-	getHeroes(): void {
+getHeroes(): void {
 		this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-//<!--		this.heroes = this.heroService.getHeroes();-->
 	}
 }
